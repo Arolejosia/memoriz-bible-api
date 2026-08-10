9d0cc1ba78ccf9f3a16a0f50d61e28b2a804fdc7
Arolejosia <92277980+Arolejosia@users.noreply.github.com>
2026-08-10 12:50:14 -0400

    Fix spelling errors in ancien_testament entries
    Corrected the spelling of 'Psaumes' to 'Psaume' and 'Cantique des Cantiques' to 'Cantique des Cantique'.

--- game_routes.py
+++ game_routes.py
@@ -70,7 +70,7 @@ BOOK_GROUPS = {
         "fr": [
             "Genèse", "Exode", "Lévitique", "Nombres", "Deutéronome", "Josué", "Juges", "Ruth", 
             "1 Samuel", "2 Samuel", "1 Rois", "2 Rois", "1 Chroniques", "2 Chroniques", "Esdras", 
-            "Néhémie", "Esther", "Job", "Psaumes", "Proverbes", "Ecclésiaste", "Cantique des Cantiques", 
+            "Néhémie", "Esther", "Job", "Psaume", "Proverbes", "Ecclésiaste", "Cantique des Cantique", 
             "Ésaïe", "Jérémie", "Lamentations", "Ézéchiel", "Daniel", "Osée", "Joël", "Amos", "Abdias", 
             "Jonas", "Michée", "Nahum", "Habacuc", "Sophonie", "Aggée", "Zacharie", "Malachie"
         ],
